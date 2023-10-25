62d258a1b7db66d7ab1c1aea54cc45d2cb241c30
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2023-10-25 21:19:40 +0100

    another cahnge

--- server/app.js
+++ server/app.js
@@ -2,7 +2,20 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 app.get('/',(req,res)=>{
-    res.send("hello")
+    res.setHeader('Content-Type','text/plain')
+    var d= new Date()
+    console.log(req.query.Name)
+    //console.log(req.originalUrl)
+    //console.log(req.get('host'))
+
+    
+    //termine la réponse HTTP sans envoyer des données
+    res.end(d.toString()+'Bonjour')
+})
+app.use((req,res)=>{
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
 })
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
