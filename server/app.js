5b873c21941826bf7574aac17c299cc0dd1b350b
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2023-10-29 13:09:00 +0100

    this is the  backend stuff we add

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+mongoose.set('strictQuery',false)
+/*
+when you perform queries using Mongoose,
+ you can specify fields that are not defined in the schema,
+  and Mongoose will not throw an error if these fields are present
+   in the documents being queried. */
 const fs = require('fs')
 const app = express()
+const PORT = process.env.PORT
 app.get('/',(req,res)=>{
     res.setHeader('Content-Type','text/plain')
     var d= new Date()
@@ -17,6 +27,16 @@ app.use((req,res)=>{
     res.status(404)
     res.send('404 - Not Found')
 })
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
+
+
+const start = async()=>{
+    try{
+        await mongoose.connect('mongodb+srv://zainabjinari:PnLBSm1LawoQw9en@cluster0.zmyj8km.mongodb.net/?retryWrites=true&w=majority');
+        app.listen(PORT,()=>{
+            console.log("server is running on port"+PORT)
+        })
+    }catch(e){
+        console.log(e.message)
+    }
+}
+start();
